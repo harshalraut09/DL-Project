7cedc6a9d83f5d2026ab337387f8dce69021db6b
unknown <hraut513@gmail.com>
2025-03-18 15:05:43 +0530

    Model Evaluation Completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,7 +2,7 @@ from src.cnnClassifier.constants import *
 import os
 from src.cnnClassifier.utils.common import read_yaml, create_directories, save_json
 
-from src.cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig)
+from src.cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig,EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -80,5 +80,17 @@ class ConfigurationManager:
 
         return training_config
     
+    
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://github.com/harshalraut09/DL-Project.git",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
 
 
